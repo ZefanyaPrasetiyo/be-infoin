e4f5b00847cd451823e144072c0fe5d508cb7dac
ZefanyaLauPrasetiyo <zefanyaprasetiyo29@gmail.com>
2026-05-16 23:16:12 +0700

    feat: test pest php

---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
