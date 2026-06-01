8624e059a46d5b28f34babcb2816bf2df8ea786d
ZefanyaLauPrasetiyo <zefanyaprasetiyo29@gmail.com>
2026-06-01 22:41:28 +0700

    chore: change api route in bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,13 +7,18 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use App\Http\Middleware\ApiKeyMiddleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
+   ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::prefix('api')
+                ->middleware('api') // Hanya pakai middleware dasar API, gak pake auth global bray!
+                ->group(__DIR__.'/../routes/api.php');
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
